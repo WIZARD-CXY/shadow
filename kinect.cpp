2669ad121bf097d77ec1774300e8b599001b6d64
WizardCXY <wizard_cxy@hotmail.com>
2012-08-14 22:44:04 +0800

    change a little bit in kinect.cpp

--- kinect.cpp
+++ kinect.cpp
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
         if (!motor.Open()) // Open motor device
                 return 1;
        
-		int i=40;
+		int i=50;
 		while(i>=-40)
 		{
 			motor.Move(i);
